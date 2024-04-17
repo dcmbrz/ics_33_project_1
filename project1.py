cf6905fff77f7ede5d25cbef51ae3262755c336c
Michael Ekwueme <ekwuemem@uci.edu>
2024-04-17 12:10:06 -0700

    created my propagation class and cancellation class

--- project1.py
+++ project1.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 
 class Alert:
-    def __init__(self, device, messge, time_recieved):
+    def __init__(self, device, message, time_received):
         self.device = device
-        self.messge = messge
-        self.time_recieved = time_recieved
+        self.message = message
+        self.time_received = time_received
+class Propagation:
+    def __init__(self, call, device1, device2, time_received):
+        self.call = call
+        self.device1 = device1
+        self.device2 = device2
+        self.time_received = time_received
+class Cancellation:
+    def __init__(self, call, device, message, time_received):
+        self.call = call
+        self.device = device
+        self.message = message
+        self.time_received = time_received
 
 
 def _read_input_file_path() -> Path:
@@ -23,6 +35,7 @@ def main() -> None:
     alert_lst= []
     time = []
     cancellations = []
+    cancellations_lst= []
     input_file_path = _read_input_file_path()
     with open(input_file_path, 'r+') as file:
         for line in file:
@@ -38,15 +51,25 @@ def main() -> None:
                 cancellations.append(line)
 
 
-
     for alert in alerts:
         alert_parts= alert.split()
         alert_lst.append(Alert(alert_parts[1], alert_parts[2], alert_parts[3]))
-    print(alert_lst[0].time_recieved)
+    #print(alert_lst[0].time_received)
 
-    #print(devices, propagations, alerts, cancellations)
-    #print(propagations)
+    for propagation in propagations:
+        propagation_list.append(Propagation(propagation[0], propagation[1], propagation[2], propagation[3]))
+    #print(propagation_list[0].call)
+    #print(propagation_list[0].device1)
+    #print(propagation_list[0].device2)
+    #print(propagation_list[0].time_received)
 
+    for cancellation in cancellations:
+        cancellation_parts= cancellation.split()
+        cancellations_lst.append(Cancellation(cancellation_parts[0], cancellation_parts[1], cancellation_parts[2], cancellation_parts[3]))
+    #print(cancellations_lst[0].call)
+    #print(cancellations_lst[0].device)
+    #print(cancellations_lst[0].message)
+    #print(cancellations_lst[0].time_received)
 
 
 
