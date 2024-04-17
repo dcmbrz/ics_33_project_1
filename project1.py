4ac78127485f9e3f51f03e4ca49ddea47544665f
Michelle Ekwueme <michelle.e@berkeley.edu>
2024-04-16 21:02:35 -0700

    I created a way to separate all of the different messages in the input file

--- project1.py
+++ project1.py
@@ -1,17 +1,41 @@
 from pathlib import Path
 
 
+
+
+
 def _read_input_file_path() -> Path:
     """Reads the input file path from the standard input"""
     return Path(input())
 
 
+
 def main() -> None:
     """Runs the simulation program in its entirety"""
+    lines = []
+    devices = []
+    propagations = []
+    alerts = []
+    time = []
+    cancellations = []
     input_file_path = _read_input_file_path()
-    with open(input_file_path, 'r') as file:
+    with open(input_file_path, 'r+') as file:
         for line in file:
-            print('lines:',line)
+            lines.append(line.strip('\n'))
+        for line in lines:
+            if line.startswith('DEVICE'):
+                devices.append(line)
+            if line.startswith('PROPAGATE'):
+                propagations.append(line)
+            if line.startswith('ALERT'):
+                alerts.append(line)
+            if line.startswith('CANCEL'):
+                cancellations.append(line)
+
+
+    #print(lines)
+    print(devices, propagations, alerts, cancellations)
+
 
 
 
