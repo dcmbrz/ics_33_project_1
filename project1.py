776d8a1983edb64e7d53c3a8608d83c950647c7f
Michael Ekwueme <ekwuemem@uci.edu>
2024-04-17 17:39:34 -0700

    added more to propagation

--- project1.py
+++ project1.py
@@ -11,12 +11,27 @@ class Propagation:
         self.device1 = device1
         self.device2 = device2
         self.time_received = time_received
+
+
+    # def propagation_set(self, device1, device2, time_received):
+    #     self.sources[device1] = (device2, time_received)
+
+
+
 class Cancellation:
     def __init__(self, call, device, message, time_received):
         self.call = call
         self.device = device
         self.message = message
         self.time_received = time_received
+class Timeline:
+    def __init__(self, event):
+        self.event = event
+
+    def run(self):
+        while self.event:
+            pass
+
 
 
 def _read_input_file_path() -> Path:
@@ -33,10 +48,13 @@ def main() -> None:
     propagation_list= []
     alerts = []
     alert_lst= []
-    time = []
     cancellations = []
     cancellations_lst= []
     input_file_path = _read_input_file_path()
+    length_list= []
+    length= 0
+    sources = {}
+    current_time= 0
     with open(input_file_path, 'r+') as file:
         for line in file:
             lines.append(line.strip('\n'))
@@ -49,15 +67,26 @@ def main() -> None:
                 alerts.append(line)
             elif line.startswith('CANCEL'):
                 cancellations.append(line)
+            elif line.startswith('LENGTH'):
+                length_list.append(line)
+                l= line.split()
+                length= int(l[1])
 
 
     for alert in alerts:
         alert_parts= alert.split()
         alert_lst.append(Alert(alert_parts[1], alert_parts[2], alert_parts[3]))
+    #print(alert_lst[0].device)
     #print(alert_lst[0].time_received)
 
     for propagation in propagations:
+        current_time += int(propagation[3])
         propagation_list.append(Propagation(propagation[0], propagation[1], propagation[2], propagation[3]))
+        sources[propagation[1]] = (propagation[2],current_time)
+    print(sources)
+
+
+
     #print(propagation_list[0].call)
     #print(propagation_list[0].device1)
     #print(propagation_list[0].device2)
@@ -70,6 +99,15 @@ def main() -> None:
     #print(cancellations_lst[0].device)
     #print(cancellations_lst[0].message)
     #print(cancellations_lst[0].time_received)
+
+    for i in range(length):
+        for k,v in sources.items():
+            if v[1] == i:
+                print(f'#{k} SENT ALERT TO #{v[0]}: Trouble')
+
+
+
+
 
 
 
